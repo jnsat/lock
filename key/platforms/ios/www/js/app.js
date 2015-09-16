54d428550863736fe29f5c7038a7b288aa42ea99
Zachary Holland <joshua@cubeolving.com>
2015-09-15 21:02:41 -0400

    try to apply gitignore to tracked files

--- key/platforms/ios/www/js/app.js
+++ key/platforms/ios/www/js/app.js
@@ -1,19 +1,21 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
-
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
+angular.module('key', ['ionic'])
+//.factory('udpserv')
+.controller('keyctrl', ['$scope', 'chrome', function($scope, chrome) {
+  //hgfjhgf
+}])
+  .run(function($ionicPlatform, $timeout) {
+    $ionicPlatform.ready(function() {
+    alert('window.cordova: ' + Object.keys(window.cordova).join(', '));
+    alert("printed windows.cordova");
+    // $scope.sendcmd = function sendcmd(cmd) {
+    //   console.log("cmd: " + cmd);
+    //   chrome.sockets.udp.create({}, function(socketInfo) {
+    //     var socketId = socketInfo.socketId;
+    //     chrome.sockets.udp.send(socketId, cmd,
+    //       '10.0.0.9', 2390, function(sendInfo) {
+    //         console.log("sent " + sendInfo.bytesSent);
+    //     });
+    //   });
+    // };
   });
-})
+});
