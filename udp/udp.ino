2cfd5aa35c82f888d1e921974a1717001164a7be
Joshua Satterfield <joshua@cubesolving.com>
2015-11-27 14:31:47 -0500

    fix wifi and add servo

--- udp/udp.ino
+++ udp/udp.ino
@@ -1,129 +1,50 @@
 /*
- Arduino Wireless Lock by Joshua And Marcus
  Run this on an Arduino with a WiFi Shield.
  in a terminal run
 nc -u [shield's ip] 2390
   e.g.
 nc -u 192.168.1.129 2390
- send l to sim. lock and u to sim. unlock. a light will blink.
+ send l to sim. lock and u to sim. unlock. a light will blink. 
 
  based on:
  WiFi UDP Send and Receive String
  This sketch wait an UDP packet on localPort using a WiFi shield.
  When a packet is received an Acknowledge packet is sent to the client on port remotePort
+
  created 30 December 2012
  by dlf (Metodo2 srl)
+ blinking for testing added by Joshua
 */
 #include <SPI.h>
 #include <WiFi.h>
 #include <WiFiUdp.h>
-#include <AES.h>
 #include <Servo.h>
-Servo servo;
-#define OPEN 0 /* degrees */
-#define CLOSE 180
-/* pwm length. current for 1 millisecs signals clockwise, etc.  */
-enum pwm { cw = 1, ccw = 2, motor = 7 };
+
+enum {
+  LOCK = 100, UNLOCK = 0, MOTOR = 9
+};
+
 long randnum;
 int status = WL_IDLE_STATUS;
 char ssid[] = "Cisco25707";  //  your network SSID (name)
 char pass[] = "password";    // your network password
-int keyIndex = 0;            // your network key Index number (needed only for WEP)
 unsigned int localPort = 2390;      // local port to listen on
+
 char packetBuffer[255]; //buffer to hold incoming packet
-char  ReplyBuffer[] = "ACK";       // a string to send back
+char  ReplyBuffer[] = "acknowledged";       // a string to send back
+Servo servo;
 WiFiUDP Udp;
-AES aes ;
 
-byte key[] =
-{
-  0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-} ;
-
-byte plain[] =
-{
-  // 0xf3, 0x44, 0x81, 0xec, 0x3c, 0xc6, 0x27, 0xba, 0xcd, 0x5d, 0xc3, 0xfb, 0x08, 0xf2, 0x73, 0xe6
-  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-  0xC0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-  0xE0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-  0xF0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-} ;
-
-byte my_iv[] =
-{
-  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01,
-} ;
-
-byte cipher [4 * N_BLOCK] ;
-byte check [4 * N_BLOCK] ;
-
-/* to go clockwise use CW, which has the value of 1,
- *  and gives a 1 millisec pusle. For CCW use CCW...
- */
-void pulse(int dir)
-{
-  digitalWrite(motor, HIGH);
-  delay(dir); /* wait the assigned amount for the pwm */
-  digitalWrite(motor, LOW);
-}
-
-void prekey (int bits, int blocks)
-{
-  byte iv [N_BLOCK] ;
-
-  long t0 = micros () ;
-  byte succ = aes.set_key (key, bits) ;
-  long t1 = micros() - t0 ;
-  Serial.print ("set_key ") ; Serial.print (bits) ; Serial.print (" ->") ; Serial.print ((int) succ) ;
-  Serial.print (" took ") ; Serial.print (t1) ; Serial.println ("us") ;
-  t0 = micros () ;
-  if (blocks == 1)
-    succ = aes.encrypt (plain, cipher) ;
-  else
-  {
-    for (byte i = 0 ; i < 16 ; i++)
-      iv[i] = my_iv[i] ;
-    succ = aes.cbc_encrypt (plain, cipher, blocks, iv) ;
-  }
-  t1 = micros () - t0 ;
-  Serial.print ("encrypt ") ; Serial.print ((int) succ) ;
-  Serial.print (" took ") ; Serial.print (t1) ; Serial.println ("us") ;
-
-  t0 = micros () ;
-  if (blocks == 1)
-    succ = aes.decrypt (cipher, plain) ;
-  else
-  {
-    for (byte i = 0 ; i < 16 ; i++)
-      iv[i] = my_iv[i] ;
-    succ = aes.cbc_decrypt (cipher, check, blocks, iv) ;
-  }
-  t1 = micros () - t0 ;
-  Serial.print ("decrypt ") ; Serial.print ((int) succ) ;
-  Serial.print (" took ") ; Serial.print (t1) ; Serial.println ("us") ;
-
-  for (byte ph = 0 ; ph < (blocks == 1 ? 3 : 4) ; ph++)
-  {
-    for (byte i = 0 ; i < (ph < 3 ? blocks*N_BLOCK : N_BLOCK) ; i++)
-    {
-      byte val = ph == 0 ? plain[i] : ph == 1 ? cipher[i] : ph == 2 ? check[i] : iv[i] ;
-      Serial.print (val >> 4, HEX) ; Serial.print (val & 15, HEX) ; Serial.print (" ") ;
-    }
-    Serial.println () ;
-  }
-
-}
-  
 void setup() {
+  servo.attach(MOTOR);
+  //Initialize serial and wait for port to open:
   Serial.begin(9600);
   randomSeed(analogRead(0)); /* seed with analog pin noise */
-  servo.attach(motor);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for Leonardo only
   }
-  
-  //// check for the presence of the shield:
+
+  // check for the presence of the shield:
   if (WiFi.status() == WL_NO_SHIELD) {
     Serial.println("WiFi shield not present");
     // don't continue:
@@ -131,7 +52,6 @@ void setup() {
   }
 
   String fv = WiFi.firmwareVersion();
-  Serial.println(fv);
   if (fv != "1.1.0") {
     Serial.println("Please upgrade the firmware");
   }
@@ -154,7 +74,7 @@ void setup() {
 }
 
 void loop() {
-    // if there's data available, read a packet
+  // if there's data available, read a packet
   int packetSize = Udp.parsePacket();
   if (packetSize) {
     Serial.print("Received packet of size ");
@@ -172,14 +92,16 @@ void loop() {
     Serial.println(packetBuffer);
     char c = packetBuffer[0];
     if (c == 'l') /* lock */
-      servo.write(180);
-    else if (c == 'u') /* unlock */
-      servo.write(0);
+      servo.write(LOCK);
+    else if (c == 'u') { /* unlock */
+      servo.write(UNLOCK);
+      Serial.println("unlock");
+    }
     else if (c == 'r') {
       randnum = random(999999);
       Serial.println(randnum);
     } else {
-      Serial.print("err: did not understand command: ");
+      Serial.print("err: did not understand command:->");
       Serial.print(packetBuffer);
       Serial.print("<-\n");
     }
@@ -209,4 +131,3 @@ void printWifiStatus() {
   Serial.print(rssi);
   Serial.println(" dBm");
 }
-
